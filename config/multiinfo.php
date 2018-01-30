afa3a5e702707e76229c36f49b6ba0237dc803fd
Krzysztof Grabania <krzysztof.grabania@gmail.com>
2018-01-30 15:48:14 +0100

    Read config data from .env file

--- config/multiinfo.php
+++ config/multiinfo.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'url'        => 'https://api1.multiinfo.plus.pl', // API base URL
+    'url'        => env('MULTIINFO_URL', 'https://api1.multiinfo.plus.pl'), // API base URL
 
-    'login'      => null, // API user login
-    'password'   => null, // API user password
-    'service_id' => null, // API user service id
+    'login'      => env('MULTIINFO_LOGIN'),      // API user login
+    'password'   => env('MULTIINFO_PASSWORD'),   // API user password
+    'service_id' => env('MULTIINFO_SERVICE_ID'), // API user service id
 
     'cert'       => [
-        'path'     => null,  // Certificate absolute path on server
-        'password' => null,  // Certificate password
-        'type'     => 'P12', // Certificate type (PEM or P12)
+        'path'     => env('MULTIINFO_CERT_PATH'),        // Certificate absolute path on server
+        'password' => env('MULTIINFO_CERT_PASSWORD'),    // Certificate password
+        'type'     => env('MULTIINFO_CERT_TYPE', 'P12'), // Certificate type (PEM or P12)
     ],
 ];
