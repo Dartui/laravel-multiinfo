0928629cb385ccf01f86d99f27b11431028241d6
Krzysztof Grabania <krzysztof.grabania@gmail.com>
2018-04-09 11:17:44 +0200

    Add timeout to GetSms request

--- src/Http/Requests/GetSmsRequest.php
+++ src/Http/Requests/GetSmsRequest.php
@@ -6,4 +6,20 @@ class GetSmsRequest extends Request
     protected $action = 'getSms';
 
     protected $url = 'getsms.aspx';
+
+    protected $timeout;
+
+    public function setTimeout($timeout)
+    {
+        $this->timeout = $timeout;
+
+        return $this;
+    }
+
+    public function toArray()
+    {
+        return [
+            'timeout' => $this->timeout,
+        ];
+    }
 }
