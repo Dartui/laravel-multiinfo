4b6275e79aa0d62184d8cbeb7d897709b03dbd2b
Krzysztof Grabania <krzysztof.grabania@gmail.com>
2018-04-24 10:53:00 +0200

    Remove methods from interfaces

--- src/Contracts/Request.php
+++ src/Contracts/Request.php
@@ -3,15 +3,5 @@ namespace Dartui\Multiinfo\Contracts;
 
 interface Request
 {
-    public function send();
-
-    public function action();
-
-    public function client();
-
-    public function url();
-
-    public function parameters();
-
-    public function toArray();
+    //
 }
